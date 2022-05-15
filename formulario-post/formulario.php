7bed56b04714e39715f8ac00917f7c6a875f67c3
Pau Egea <papau84@gmail.com>
2022-05-15 20:04:11 +0200

    formularios post

---
+++ formulario-post/formulario.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<form action="server.php" method="post">
+        <label for="nombre">Nombre: </label>
+        <input type="text" name="nombre" id="nombre" placeholder="Introduce tu nombre">
+        
+        <label for="edad">Edad: </label>
+        <input type="number" name="edad" id="edad" placeholder="Introduce tu edad">
+        <button type="submit">Mandar formulario</button>
+    </form>
+</body>
+</html>
